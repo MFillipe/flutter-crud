d1bc1af310b23cc1e62b57dfd38a144d55abaa35
Matheus Filipe <matheus_filipe00@outlook.com>
2021-05-04 14:23:08 -0300

    feat: add userForms and validator to create a new user

--- lib/views/user_form.dart
+++ lib/views/user_form.dart
@@ -1,12 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_crud/models/user.dart';
+import 'package:flutter_crud/provider/users.dart';
+import 'package:provider/provider.dart';
 
 class UserFrom extends StatelessWidget {
+  final _form = GlobalKey<FormState>();
+  final Map<String, String> _formData = {};
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('User Forms'),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.save),
+              onPressed: () {
+                final isValid = _form.currentState.validate();
+
+                if (isValid) {
+                  _form.currentState.save();
+
+                  Provider.of<Users>(context, listen: false).put(
+                    User(
+                      id: _formData['id'],
+                      name: _formData['name'],
+                      email: _formData['email'],
+                      avatarUrl: _formData['avatarUrl'],
+                    ),
+                  );
+
+                  Navigator.of(context).pop();
+                }
+              })
+        ],
       ),
+      body: Padding(
+          padding: EdgeInsets.all(15),
+          child: Form(
+            key: _form,
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Name'),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Name is empty';
+                    }
+
+                    if (value.trim().length < 3) {
+                      return 'Name too short. Must have more than 3.';
+                    }
+
+                    return null;
+                  },
+                  onSaved: (value) => _formData['name'] = value,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'E-mail'),
+                  onSaved: (value) => _formData['email'] = value,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Avatar URL'),
+                  onSaved: (value) => _formData['avatarUrl'] = value,
+                )
+              ],
+            ),
+          )),
     );
   }
 }
